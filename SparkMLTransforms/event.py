bae7b7f5e300c3f8848ba59151c013dccde1508a
Tianle Chen <tianlechen@gmail.com>
2022-07-16 10:29:03 -0400

    event

---
+++ SparkMLTransforms/event.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+from datetime import datetime
+from typing import List, Optional
+
+import pyspark.sql.functions as F
+from pyspark.sql import DataFrame
+from pyspark.sql.types import DoubleType, StructField, StructType
+from stpd.event import Holiday
+
+from SparkMLTransforms.base import IdentityFitTransformer
+
+
+class HolidayFeatures(IdentityFitTransformer):
+    def __init__(self, input_columns: List[str], params: Optional[dict] = None) -> None:
+        self.input_columns = input_columns
+        self.params = params
+        self.f = Holiday(**params)
+
+    def transform(self, df: DataFrame):
+        for c in self.input_columns:
+            sample_output_value = self.f(datetime(self.params.get('years')[0], 1, 1))
+            mapping = {
+                row[c]: self.f(row[c])
+                for row in df.select(c).distinct().collect()
+            }
+            udf_return_schema = StructType([
+                StructField(k, DoubleType(), False)
+                for k in sample_output_value
+            ])
+            udf = F.udf(lambda v: mapping.get(v), udf_return_schema)
+            df = df.withColumn(self.input_output_columns[c], udf(c)).drop(c)
+        return df
---
+++ tests/test_event.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+import pandas as pd
+from pyspark.sql import SparkSession
+
+from SparkMLTransforms.event import HolidayFeatures
+from SparkMLTransforms.utils import unpack_structtype_column
+
+
+def test_HolidayFeatures(spark: SparkSession):
+    input_df = pd.DataFrame({'dt': [datetime(2022, 1, 1)]})
+    input_df = spark.createDataFrame(input_df)
+
+    params = {
+        'years': [2021, 2022, 2023],
+        'country': 'US'
+    }
+    output_df = (
+        HolidayFeatures(input_columns=['dt'], params=params)
+        .fit(input_df)
+        .transform(input_df)
+    )
+    output_df = unpack_structtype_column(output_df, 'dt_HolidayFeatures')
+    assert output_df.columns == [
+        'dt_HolidayFeatures_us_holiday_days_since_last',
+        'dt_HolidayFeatures_us_holiday_days_to_next'
+    ]
